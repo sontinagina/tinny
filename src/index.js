0bae4d2e729db0fd71a942bf7fc42d9a5fddb79b
sontinagina <sontinagina11@gmail.com>
2021-11-28 01:45:58 +0530

    issue fixed

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const cors = require("cors");
 const express = require("express");
 const app = express();
-const URLs = "https://tinnys.herokuapp.com/u/";
-// const URLs="http://localhost:3002/u/";
-const URLs2 = "https://tinnys.herokuapp.com";
-// const URLs2="http://localhost:3002";
-app.use(cors());
+// const URLs = "https://tinnys.herokuapp.com/u/";
+const URLs="http://localhost:3002/u/";
+// const URLs2 = "https://tinnys.herokuapp.com";
+const URLs2="http://localhost:3002";
+app.use(cors({
+    origin:"http://localhost:3001",
+    credentials:true,
+}));
 app.use(express.json());
 const mongoose = require("mongoose");
 const conn_url =
